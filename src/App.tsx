1c5852f1d69f6a1f0997b19d4b72afba4c832f5c
Edward Selirah <prinzeedward@gmail.com>
2021-06-21 08:02:17 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Store } from 'redux'
+import { ApplicationState } from 'redux/root-reducer'
+import FallbackSpinner from 'core/components/spinner/fallback'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  store: Store<ApplicationState>
+  persistor: any
 }
 
-export default App;
+const App: React.FC<AppProps> = ({ store, persistor }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <h1>App</h1>
+        <FallbackSpinner />
+      </PersistGate>
+    </Provider>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { configureStore } from 'redux/store'
+
+declare global {
+  interface Window {
+    INITIAL_REDUX_STATE: any
+  }
+}
+
+const initialState = window.INITIAL_REDUX_STATE
+
+export const { store, persistor } = configureStore(initialState)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} persistor={persistor} />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
