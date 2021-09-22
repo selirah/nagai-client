e57f66dbae066d99a374bc86896b88f25cc9b8e1
Edward Selirah <prinzeedward@gmail.com>
2021-09-22 17:13:22 +0000

    order init

---
+++ src/pages/orders/routes.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OrdersRoutes = () => {
+  return <div></div>
+}
+
+export default OrdersRoutes
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -77,3 +77,11 @@ export const getManufacturer = (id: number) => {
 }
 
 export const isUserLoggedIn = () => localStorage.getItem('user')
+
+export const generateReadableNumbers = () => {
+  let now = Date.now().toString() // '1492341545873'
+  // pad with extra random digit
+  now += now + Math.floor(Math.random() * 10)
+  // format
+  return [now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-')
+}
